58487e0f1dcc413c6bbd17d497b908827658340a
Maish Saidel-Keesing <maishsk@gmail.com>
2015-11-16 23:04:32 +0100

    restored nmafr

--- cloudflare.tf
+++ cloudflare.tf
@@ -28,13 +28,13 @@ resource "cloudflare_record" "maishsk.com_nmade" {
     ttl = 1
 }
 
-#resource "cloudflare_record" "maishsk.com_nmafr" {
-#    domain = "${var.cloudflare_domain}"
-#    name = "maishsk.com"
-#    value = "188.165.232.215"
-#    type = "A"
-#    ttl = 1
-#}
+resource "cloudflare_record" "maishsk.com_nmafr" {
+    domain = "${var.cloudflare_domain}"
+    name = "maishsk.com"
+    value = "188.165.232.215"
+    type = "A"
+    ttl = 1
+}
 
 resource "cloudflare_record" "maishsk.com_nmakc" {
     domain = "${var.cloudflare_domain}"
