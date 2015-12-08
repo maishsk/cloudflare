ae5e520a906363e960543ba3e39d05a69bf6eafe
Maish Saidel-Keesing <maishsk@gmail.com>
2015-12-08 16:43:46 +0200

    fixed nmakc outage

--- cloudflare.tf
+++ cloudflare.tf
@@ -36,13 +36,13 @@ resource "cloudflare_record" "maishsk.com_nmafr" {
     ttl = 1
 }
 
-#resource "cloudflare_record" "maishsk.com_nmakc" {
-#    domain = "${var.cloudflare_domain}"
-#    name = "maishsk.com"
-#    value = "192.187.97.130"
-#    type = "A"
-#    ttl = 1
-#}
+resource "cloudflare_record" "maishsk.com_nmakc" {
+    domain = "${var.cloudflare_domain}"
+    name = "maishsk.com"
+    value = "192.187.97.130"
+    type = "A"
+    ttl = 1
+}
 
 resource "cloudflare_record" "maishsk.com_nmanc" {
     domain = "${var.cloudflare_domain}"
